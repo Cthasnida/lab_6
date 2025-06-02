9415221a6df9536800bd1141164bcd3a9287534c
Siti Hasnida <sitihasnida11@gmail.com>
2025-06-02 13:30:11 +0800

    LAB 6 Q3

---
+++ lab6_q3.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        function calculateArea($width, $height) {
+        return $width * $height;
+        }
+
+        $width = 4;
+        $height = 2;
+        $area = calculateArea($width, $height);
+
+        echo "The area of rectangle with a width of $width and $height is $area";
+    ?>
+</body>
+</html>
