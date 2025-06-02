72105e8d3424ef344eb590597e71d2e1e8de30be
Siti Hasnida <sitihasnida11@gmail.com>
2025-06-02 13:29:49 +0800

    LAB 6 Q1

---
+++ lab6_q1.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Lab 6 Q1</title>
+</head>
+<body>
+    <?php
+        $name = "Siti Hasnida";
+        $MatricNumber = "AI230135";
+        $Course = "BIT";
+        $YearofStudy = "3";
+        $Address = "No 33, Jalan Meranti 3A/11, 44300 Batang Kali, Selangor";
+    
+    ?>
+ <table border="1">
+    <tr>
+        <td>Name</td>
+        <td><?php echo "$name"; ?></td>
+    </tr>
+    <tr>
+        <td>Matric Number</td>
+        <td><?php echo "$MatricNumber"; ?></td>
+    </tr>
+    <tr>
+        <td>Course</td>
+        <td><?php echo "$Course"; ?></td>
+    </tr>
+    <tr>
+        <td>Year Of Study</td>
+        <td><?php echo "$YearofStudy"; ?></td>
+    </tr>
+    <tr>
+        <td>Address</td>
+        <td><?php echo "$Address"; ?></td>
+    </tr>
+
+ </table>
+
+</body>
+</html> 
